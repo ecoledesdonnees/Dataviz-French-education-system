7b85570f800b67990315b5fa2b055286aaaaead1
Victorin Martin <victorin.martin@curie.fr>
2015-03-30 17:48:23 +0200

    removing the need of the state definition file.

--- data/generate_tree.py
+++ data/generate_tree.py
@@ -3,13 +3,9 @@ import igraph
 import re
 import sys
 
-# will define the scholar_tree in json format from two files:
-# 'states.dat' which contains the list of possible states (could be removed). 
-# 'transitions.dat' stroing the possible transitions btw states. 
-# the second file should be 'tab separated' while on the first one,
-# one state per line is expected.
-# defining transition to 'Drop' state for every state after '3eme' can be
-# easily done using the graph 'g'.    
+# will define the scholar_tree in json format from one file:
+# 'transitions.dat' storing the possible transitions btw states.
+# this file should be 'tab separated'.
 
 def line_header(state,b):
 	if b:
@@ -22,7 +18,7 @@ def repeat_header():
 
 def leads_to():
 	return "\"leadsTo\": [\n"
-	
+
 def do_next_level(g,vertex,json,level,last):
 	succ = g.successors(vertex)
 	ts  = "\t"*level
@@ -33,11 +29,10 @@ def do_next_level(g,vertex,json,level,last):
 	if(len(succ)!=0):
 		json.write( ts_ + repeat_header() )
 		json.write( ts_ + leads_to() )
-		#print(succ)
 		for s in succ:
 			do_next_level(g,s,json,level+2,s==succ[-1])
-		json.write( ts_ + "]\n" ) 
-	if last:	
+		json.write( ts_ + "]\n" )
+	if last:
 		json.write( ts + "}\n" )
 	else:
 		json.write( ts + "},\n" )
@@ -47,17 +42,21 @@ states = open("states.dat","r")
 transitions = open("transitions.dat","r")
 json = open("orientation_tree.json","w")
 
-#read the graph from the files 'states.dat' & 'transitions.dat'. 
+#read the graph from the files 'states.dat' & 'transitions.dat'.
 g = igraph.Graph()
 igraph.Graph.to_directed(g)
-for line in states:
-	g.add_vertex(line.rstrip())
+g.add_vertex("Drop")
 
 for line in transitions:
 	st = line.split('\t')
+	if (not (st[0] in g.vs["name"])):
+		g.add_vertex(st[0])
+	if (not (st[1] in g.vs["name"])):
+		g.add_vertex(st[1].rstrip())
 	g.add_edge(st[0],st[1].rstrip())
 
-#add edge to 'Drop' from every level >=3eme
+
+#add edge to 'Drop' from every level >= 3eme [except achievement levels].
 states = [ g.vs["name"].index("3eme") ]
 drop = g.vs["name"].index("Drop")
 while(len(states)!=0):
@@ -68,10 +67,8 @@ while(len(states)!=0):
 		g.add_edge(c_state,drop)
 	states.pop(0)
 
-#print(g)
-
-#
-do_next_level(g,0,json,0,"true")
-#
+#generating the orientation tree (which is a DAG).
+init = g.vs["name"].index("CP") #initial point.
+do_next_level(g,init,json,0,"true")
 
 
